ef128dc4c6b6560ebe3fdae98f9b84704356e20b
luc de weerdt <luc@dewetho.be>
2020-08-10 16:02:42 +0200

    niet slecht lucske

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'ask', to: "questions#ask"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'answer', to: "questions#answer"
+  
 end
